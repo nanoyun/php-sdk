d20d4a9cd8edbf71d25dc1993a4d4c7b05a4593d
hackingangle <hackingangle@gmail.com>
2013-10-24 14:03:30 +0800

    公网环境配置

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$app_key = '5138198270'; //App key
+$app_key = '5138200455'; //App key
 $app_secret = 'e4da3b7fbbce2345d7772b0674a318d5';//App Secret
 date_default_timezone_set('Asia/Shanghai'); //定义系统默认时间区域
